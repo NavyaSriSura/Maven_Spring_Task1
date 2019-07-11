49ef19c2d9050acf58baf7bb61313bcbc0e12c39
NavyaSriSura <navyasura98@gmail.com>
2019-07-11 17:29:47 +0530

    task4

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,7 +1,18 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements BeanFactoryAware, BeanNameAware, ApplicationContextAware {
     Actor actor;
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
 
 
     public Actor getActor() {
@@ -17,5 +28,20 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println(s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
     }
 }
--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -11,16 +11,19 @@ import java.sql.SQLOutput;
 
 public class main {
     public static void main(String[] args) {
-        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movieFirst = (Movie) xmlBeanFactory.getBean("movie1");
-
-
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie) applicationContext.getBean("movie2");
-
-
-        System.out.println(movieFirst.getActor());
+        Movie movie=(Movie)applicationContext.getBean("movie2");
         System.out.println(movie.getActor());
+        Movie movie1=(Movie)applicationContext.getBean("movie2",Movie.class);
+        System.out.println(movie1.getActor());
 
+        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
+        Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie2");
+
+        
+
+        movie.setApplicationContext(applicationContext);
+        movieFirst.setBeanFactory(xmlBeanFactory);
+        movie.setBeanName("beam123");
     }
 }
