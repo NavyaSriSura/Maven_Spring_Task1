0ccff21b1d36a6564a7933223cc6706c3168fa6c
NavyaSriSura <navyasura98@gmail.com>
2019-07-12 10:21:32 +0530

    task 6 first commit

--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -1,13 +1,14 @@
 package com.stackroute;
 
 import com.stackroute.Demo.BeanLifeCycleDemoBean;
+import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
-        BeanLifeCycleDemoBean beanLifeCycleDemo=(BeanLifeCycleDemoBean) applicationContext.getBean("cycle");
+       ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie=(Movie) applicationContext.getBean("movie1");
+        System.out.println(movie.getActor());
     }
 }
