648b9b22c34916d4259f62a0c8cdbe7cd9206470
NavyaSriSura <navyasura98@gmail.com>
2019-07-12 10:08:24 +0530

    task-5 first commit

--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -1,29 +1,13 @@
 package com.stackroute;
 
-import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import com.stackroute.Demo.BeanLifeCycleDemoBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-
-import java.sql.SQLOutput;
 
 public class main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=(Movie)applicationContext.getBean("movie2");
-        System.out.println(movie.getActor());
-        Movie movie1=(Movie)applicationContext.getBean("movie2",Movie.class);
-        System.out.println(movie1.getActor());
-
-        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
-        Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie2");
-
-        
-
-        movie.setApplicationContext(applicationContext);
-        movieFirst.setBeanFactory(xmlBeanFactory);
-        movie.setBeanName("beam123");
+        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
+        BeanLifeCycleDemoBean beanLifeCycleDemo=(BeanLifeCycleDemoBean) applicationContext.getBean("cycle");
     }
 }
