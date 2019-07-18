7b1c1de6c0bbac3649c1fd31701226edc0be4ce6
NavyaSriSura <navyasura98@gmail.com>
2019-07-18 16:53:14 +0530

    after adding git ignore

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,16 +1,10 @@
 package com.stackroute.domain;
 
-public class Actor {
+public class Actor
+{
     String name;
     String gender;
     int age;
-
-    public Actor(String name, String gender, int age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,20 +1,8 @@
 package com.stackroute.domain;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-public class Movie implements BeanFactoryAware, BeanNameAware, ApplicationContextAware {
+public class Movie
+{
     Actor actor;
-
-    public Movie(Actor actor) {
-        this.actor = actor;
-    }
-
-
     public Actor getActor() {
         return actor;
     }
@@ -28,20 +16,5 @@ public class Movie implements BeanFactoryAware, BeanNameAware, ApplicationContex
         return "Movie{" +
                 "actor=" + actor +
                 '}';
-    }
-
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println(beanFactory);
-    }
-
-    @Override
-    public void setBeanName(String s) {
-        System.out.println(s);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println(applicationContext);
     }
 }
